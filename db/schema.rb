98348f835d6700dc19f344ee976ad0d9e20aa6f8
Scott Slade <buzz14@gmail.com>
2015-05-27 19:33:57 -0400

    added before_filter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527224545) do
+ActiveRecord::Schema.define(version: 20150527232444) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "link"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
